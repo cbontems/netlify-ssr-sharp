9b027741a14f117a1bea4bb228a0c19ca4668ba0
Cédric Bontems <67456035+cbontems@users.noreply.github.com>
2023-09-19 15:27:19 +0200

    Add netlify.toml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: netlify({
+    dist: new URL("./dist/", import.meta.url),
+  }),
 });
